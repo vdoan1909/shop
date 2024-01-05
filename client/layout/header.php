84e961139cd455ff5bdafdeaa526d60850e95f0c
doannvph33201 <118094808+doannvph33201@users.noreply.github.com>
2024-01-05 18:47:41 +0700

    lam mobile

--- client/layout/header.php
+++ client/layout/header.php
@@ -194,138 +194,105 @@
                         </a>
                     </div>
                     <div class="block-search">
-                        <form role="search" method="get" class="form-search block-search-form akasha-live-search-form">
+                        <form action="index.php" role="search" method="get" class="form-search block-search-form akasha-live-search-form">
+                            <input type="hidden" name="url" value="ds_san_pham">
                             <div class="form-content search-box results-search">
                                 <div class="inner">
-                                    <input autocomplete="off" class="searchfield txt-livesearch input" name="s" value="" placeholder="Search here..." type="text">
+                                    <input autocomplete="off" class="searchfield txt-livesearch input" name="ten_san_pham" value="" placeholder="Tìm kiếm..." type="text">
                                 </div>
                             </div>
-                            <input name="post_type" value="product" type="hidden">
-                            <input name="taxonomy" value="product_cat" type="hidden">
                             <div class="category">
-                                <select title="product_cat" name="product_cat" class="category-search-option" tabindex="-1">
-                                    <option value="0">All Categories</option>
-                                    <option class="level-0" value="light">Shoes</option>
-                                    <option class="level-0" value="chair">Accessories</option>
-                                    <option class="level-0" value="table">Bags</option>
-                                    <option class="level-0" value="bed">Life style</option>
-                                    <option class="level-0" value="new-arrivals">New arrivals</option>
-                                    <option class="level-0" value="lamp">Summer Sale</option>
-                                    <option class="level-0" value="specials">Specials</option>
-                                    <option class="level-0" value="sofas">Women</option>
+                                <select title="product_cat" name="the_loai" id="64788262" class="category-search-option" tabindex="-1" style="display: none;">
+                                    <option value="0">Tất cả thể loại</option>
+                                    <?php foreach ($ds_the_loai as $the_loai) : ?>
+                                        <option value="<?= $the_loai["id"] ?>">
+                                            <?= $the_loai["ten_loai"] ?></option>
+                                    <?php endforeach; ?>
                                 </select>
                             </div>
                             <button type="submit" class="btn-submit">
                                 <span class="flaticon-magnifying-glass-1"></span>
                             </button>
-                        </form><!-- block search -->
+                        </form>
                     </div>
                 </div>
-                <ul class="wpml-menu">
-                    <li class="menu-item akasha-dropdown block-language">
-                        <a href="#" data-akasha="akasha-dropdown">
-                            <img src="assets/images/en.png" alt="en" width="18" height="12">
-                            English
-                        </a>
-                        <span class="toggle-submenu"></span>
-                        <ul class="sub-menu">
-                            <li class="menu-item">
-                                <a href="#">
-                                    <img src="assets/images/it.png" alt="it" width="18" height="12">
-                                    Italiano
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="menu-item">
-                        <div class="wcml-dropdown product wcml_currency_switcher">
-                            <ul>
-                                <li class="wcml-cs-active-currency">
-                                    <a class="wcml-cs-item-toggle">USD</a>
-                                    <ul class="wcml-cs-submenu">
-                                        <li>
-                                            <a>EUR</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                </ul>
             </div>
             <div class="header-mobile-mid">
                 <div class="header-logo">
-                    <a href="index.html"><img alt="Akasha" src="assets/images/logo.png" class="logo"></a>
+                    <a href="index.php"><img alt="Akasha" src="client/layout/assets/images/logo.png" class="logo"></a>
                 </div>
             </div>
             <div class="header-mobile-right">
                 <div class="header-control-inner">
                     <div class="meta-dreaming">
                         <div class="menu-item block-user block-dreaming akasha-dropdown">
-                            <a class="block-link" href="#">
-                                <span class="flaticon-profile"></span>
-                            </a>
-                            <ul class="sub-menu">
-                                <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--dashboard is-active">
-                                    <a href="#">Dashboard</a>
-                                </li>
-                                <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--orders">
-                                    <a href="#">Orders</a>
-                                </li>
-                                <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--downloads">
-                                    <a href="#">Downloads</a>
-                                </li>
-                                <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--edit-adchair">
-                                    <a href="#">Addresses</a>
-                                </li>
-                                <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--edit-account">
-                                    <a href="#">Account details</a>
-                                </li>
-                                <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--customer-logout">
-                                    <a href="#">Logout</a>
-                                </li>
-                            </ul>
+                            <?php if (isset($_SESSION["tai_khoan"])) { ?>
+                                <a class="block-link" href="#">
+                                    <span class="flaticon-profile"></span>
+                                </a>
+                                <ul class="sub-menu">
+                                    <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--dashboard is-active">
+                                        <a href="index.php?url=cap_nhat_tai_khoan">Cập nhật tài khoản</a>
+                                    </li>
+                                    <?php if ($_SESSION["tai_khoan"]["role"] == 1) { ?>
+                                        <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--customer-logout">
+                                            <a href="index.php?url=admin">Đăng nhập admin</a>
+                                        </li>
+                                    <?php } ?>
+                                    <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--customer-logout">
+                                        <a href="index.php?url=dang_xuat">Đăng xuất</a>
+                                    </li>
+                                </ul>
+                            <?php } else { ?>
+                                <a class="block-link" href="index.php?url=tai_khoan">
+                                    <span class="flaticon-profile"></span>
+                                </a>
+                                <ul style="display: none;" class="sub-menu">
+                                    <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--dashboard is-active">
+                                        <a href="index.php?url=cap_nhat_tai_khoan">Cập nhật tài khoản</a>
+                                    </li>
+                                    <li class="menu-item akasha-MyAccount-navigation-link akasha-MyAccount-navigation-link--customer-logout">
+                                        <a href="index.php?url=dang_xuat">Đăng xuất</a>
+                                    </li>
+                                </ul>
+                            <?php } ?>
                         </div>
                         <div class="block-minicart block-dreaming akasha-mini-cart akasha-dropdown">
                             <div class="shopcart-dropdown block-cart-link" data-akasha="akasha-dropdown">
-                                <a class="block-link link-dropdown" href="cart.html">
+                                <a class="block-link link-dropdown" href="#">
                                     <span class="flaticon-bag"></span>
-                                    <span class="count">3</span>
+                                    <span class="count"><?= $so_luong_san_pham_gio_hang["so_luong"] ?? 0 ?></span>
                                 </a>
                             </div>
                             <div class="widget akasha widget_shopping_cart">
                                 <div class="widget_shopping_cart_content">
-                                    <h3 class="minicart-title">Your Cart<span class="minicart-number-items">3</span>
+                                    <h3 class="minicart-title">Giỏ hàng của bạn
+                                        <span class="minicart-number-items"><?= $so_luong_san_pham_gio_hang["so_luong"] ?? 0 ?></span>
                                     </h3>
                                     <ul class="akasha-mini-cart cart_list product_list_widget">
-                                        <li class="akasha-mini-cart-item mini_cart_item">
-                                            <a href="#" class="remove remove_from_cart_button">×</a>
-                                            <a href="#">
-                                                <img src="assets/images/apro134-1-600x778.jpg" class="attachment-akasha_thumbnail size-akasha_thumbnail" alt="img" width="600" height="778">T-shirt with skirt – Pink&nbsp;
-                                            </a>
-                                            <span class="quantity">1 × <span class="akasha-Price-amount amount"><span class="akasha-Price-currencySymbol">$</span>150.00</span></span>
-                                        </li>
-                                        <li class="akasha-mini-cart-item mini_cart_item">
-                                            <a href="#" class="remove remove_from_cart_button">×</a>
-                                            <a href="#">
-                                                <img src="assets/images/apro1113-600x778.jpg" class="attachment-akasha_thumbnail size-akasha_thumbnail" alt="img" width="600" height="778">Abstract Sweatshirt&nbsp;
-                                            </a>
-                                            <span class="quantity">1 × <span class="akasha-Price-amount amount"><span class="akasha-Price-currencySymbol">$</span>129.00</span></span>
-                                        </li>
-                                        <li class="akasha-mini-cart-item mini_cart_item">
-                                            <a href="#" class="remove remove_from_cart_button">×</a>
-                                            <a href="#">
-                                                <img src="assets/images/apro201-1-600x778.jpg" class="attachment-akasha_thumbnail size-akasha_thumbnail" alt="img" width="600" height="778">Mini Dress&nbsp;
-                                            </a>
-                                            <span class="quantity">1 × <span class="akasha-Price-amount amount"><span class="akasha-Price-currencySymbol">$</span>139.00</span></span>
-                                        </li>
+                                        <?php if (isset($ds_san_pham_gio_hang)) { ?>
+                                            <?php foreach ($ds_san_pham_gio_hang as $sp_gh) :
+                                                $fm_gia = number_format($sp_gh["gia"], 0, ',', '.');
+                                                $tong_gia = $sp_gh["gia"] * $sp_gh["so_luong"];
+                                                $fm_tong_gia = number_format($tong_gia, 0, ',', '.');
+                                            ?>
+                                                <li class="akasha-mini-cart-item mini_cart_item">
+                                                    <a href="#" class="remove remove_from_cart_button">×</a>
+                                                    <a href="#">
+                                                        <img src="assets/upload/<?= $sp_gh["anh"] ?>" class="attachment-akasha_thumbnail size-akasha_thumbnail" alt="img" width="600" height="778"><?= $sp_gh["ten"] ?>&nbsp;
+                                                    </a>
+                                                    <span class="quantity"><?= $sp_gh["so_luong"] ?> × <span class="akasha-Price-amount amount"><span class="akasha-Price-currencySymbol"></span><?= $fm_gia ?></span></span>
+                                                </li>
+                                            <?php endforeach; ?>
+                                        <?php } else { ?>
+                                            <li class="akasha-mini-cart-item mini_cart_item">
+                                                <span>Không có sản phẩm trong giỏ hàng !</span>
+                                            </li>
+                                        <?php } ?>
                                     </ul>
-                                    <p class="akasha-mini-cart__total total"><strong>Subtotal:</strong>
-                                        <span class="akasha-Price-amount amount"><span class="akasha-Price-currencySymbol">$</span>418.00</span>
-                                    </p>
                                     <p class="akasha-mini-cart__buttons buttons">
-                                        <a href="cart.html" class="button akasha-forward">Viewcart</a>
-                                        <a href="checkout.html" class="button checkout akasha-forward">Checkout</a>
+                                        <a href="index.php?url=gio_hang" class="button akasha-forward">Xem
+                                            giỏ hàng</a>
                                     </p>
                                 </div>
                             </div>
